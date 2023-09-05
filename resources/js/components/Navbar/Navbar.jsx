c447583ecf152c3924a885138b0df38d470ff9c6
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-09-04 23:23:42 -0300

    Agregue el componente Navbar

---
+++ resources/js/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <h2>asdas</h2>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar
