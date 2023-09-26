12756c4c7438c600e7bb3bba5fb2b15d21c13670
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-09-26 10:02:17 -0300

    Agregue la pagina 404

---
+++ resources/js/pages/404.jsx
@@ -0,0 +1,13 @@
+import '../../css/app.css'
+const PageNotFound = () => {
+    return(
+        <div className="container not-found">
+
+            <h1>404</h1>
+            <h2>Pagina no encontrada</h2>
+            <h5>Esta seccion no existe o ya fue borrada por del administrador.</h5>
+        </div>
+    );
+}
+
+export default PageNotFound;
--- resources/js/routes/MainRouter.jsx
+++ resources/js/routes/MainRouter.jsx
@@ -1,10 +1,14 @@
 import { Routes, Router, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import PageNotFound from "../pages/404";
 
 function MainRouter(){
     return(
         <Routes>
             <Route path="/" element={ <Home /> }/>
+            <Route path="/home" element={ <Home /> }/>
+            <Route path="/*" element={ <PageNotFound /> }/>
+
         </Routes>
     )
 
